617fb0e2c8015637e0dcf1f0071a84204067e787
jose <jernestocastro01@gmail.com>
2025-05-06 11:14:09 -0400

    Implementacion y comparacion sin y con lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
+import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/posts', pathMatch: 'full' },
     { path: 'posts', component: PostListComponent },
-    { path: 'post/:id', component: PostDetailComponent }
+    { path: 'post/:id', component: PostDetailComponent },
+    { path: 'gallery', component: ImageGalleryComponent },
+    { path: 'gallery-optimized', loadComponent: () => import('./image-gallery-optimized/image-gallery-optimized.component').then(m => m.ImageGalleryOptimizedComponent) }
 ];
